35d72e2cb9e4a9c1fb91c23d6dae7704c429c0bc
Tze Ming <tzeming@botmd.io>
2020-08-17 21:14:04 +0800

    Init docker compose and env variables

--- is3106backend/is3106backend/settings.py
+++ is3106backend/is3106backend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3cme5hmt6urlh!x20ki_ln)(7z92fepj5h@o(c9klynnm^7odm'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
