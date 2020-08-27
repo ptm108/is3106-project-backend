148ceefcd00aec23ce1accc0fa9a557c34e8a0e3
Tze Ming <tzeming@botmd.io>
2020-08-28 00:37:02 +0800

    Add delete user view

--- is3106backend/users/urls.py
+++ is3106backend/users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.HelloView.as_view(), name='hello')
+  path('', views.HelloView.as_view(), name='hello'),
+  path('delete_user', views.delete_user, name="delete user")
 ]
