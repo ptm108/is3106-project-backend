89bdb631056c6c05a8bd5788001b918b2b499d1d
Tze Ming <tzeming@botmd.io>
2020-08-28 02:23:27 +0800

    Add create_user view

--- is3106backend/users/urls.py
+++ is3106backend/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
   path('', views.HelloView.as_view(), name='hello'),
+  path('create_user', views.create_user, name="create user"),
   path('delete_user', views.delete_user, name="delete user")
 ]
