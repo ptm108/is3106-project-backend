1ad606357ea7007f903dd97b2b04225012957d82
Tze Ming <tzeming.phang@u.nus.edu>
2020-09-08 20:09:14 +0800

    Init recipes app

--- is3106backend/is3106backend/settings.py
+++ is3106backend/is3106backend/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [] if os.environ.get("DJANGO_ALLOWED_HOSTS") is None else os.env
 
 INSTALLED_APPS = [
     'users',
+    'recipes',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
