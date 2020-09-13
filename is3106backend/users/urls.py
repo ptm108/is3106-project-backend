e6c8561ffcd6527b78503b513cf8ee3b01fd4889
Tze Ming <tzeming.phang@u.nus.edu>
2020-09-13 14:14:41 +0800

    Add check session endpoint

--- is3106backend/users/urls.py
+++ is3106backend/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('delete_user', views.delete_user, name='delete user'),
   path('create_delivery_address', views.create_delivery_address, name='create a delivery address'),
   path('delete_delivery_address/<slug:pk>', views.delete_delivery_address, name='delete a delivery address'),
-  path('get_delivery_addresses', views.get_delivery_addresses, name='get user delivery addresses')
+  path('get_delivery_addresses', views.get_delivery_addresses, name='get user delivery addresses'),
+  path('check_session', views.check_session, name='check validity of jwt')
 ]
