e7ec6947c423c2b9f432f4a5c67ee3327bb0fea7
eliz <e0319168@u.nus.edu>
2020-09-14 14:02:28 +0800

    added end point for get current user

--- is3106backend/users/urls.py
+++ is3106backend/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path('', views.HelloView.as_view(), name='hello'),
   path('create_user', views.create_user, name='create user'),
   path('delete_user', views.delete_user, name='delete user'),
+  path('get_current_user', views.get_current_user, name='get details of current user'),
   path('create_delivery_address', views.create_delivery_address, name='create a delivery address'),
   path('delete_delivery_address/<slug:pk>', views.delete_delivery_address, name='delete a delivery address'),
   path('get_delivery_addresses', views.get_delivery_addresses, name='get user delivery addresses')
