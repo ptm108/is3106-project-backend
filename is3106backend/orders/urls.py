f244805a1a2c094a105c7bf362c6a4c14c4f0ace
Phang Tze Ming <e0311042@u.nus.edu>
2020-10-22 22:00:21 +0800

    Update orders uri

--- is3106backend/orders/urls.py
+++ is3106backend/orders/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.groupbuy_view, name='search, filter, get all groupbuys'),
   path('/<slug:pk>', views.protected_groupbuy_view, name='Update/Approve Groupbuy'),
-  path('/orders', views.protected_order_view, name='Create Order'),
+  path('/<slug:pk>/orders', views.protected_order_view, name='Create Order'),
 ]
