029ddcb7c0cd8b55856f414c6194f26648f23934
Phang Tze Ming <e0311042@u.nus.edu>
2020-10-28 13:01:47 +0800

    Add media static urls

--- is3106backend/is3106backend/urls.py
+++ is3106backend/is3106backend/urls.py
@@ -19,6 +19,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import IndexView
 
@@ -42,4 +44,4 @@ urlpatterns = [
     # simple jwt endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
